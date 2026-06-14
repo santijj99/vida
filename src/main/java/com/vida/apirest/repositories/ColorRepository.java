cc1f1c7915b3f781ca7f5b095eab0c217292ff86
LucioArgiro <aleargiro3@gmail.com>
2026-06-14 14:47:55 -0300

    Fix: arreglos en Articulos para mostrar talles y colores de la DB

--- src/main/java/com/vida/apirest/repositories/ColorRepository.java
+++ src/main/java/com/vida/apirest/repositories/ColorRepository.java
@@ -2,9 +2,13 @@ package com.vida.apirest.repositories;
 
 import com.vida.apirest.model.articulo.Color;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ColorRepository extends JpaRepository<Color, Long> {
     Optional<Color> findByNombre(String nombre);
+    @Query("SELECT DISTINCT c.nombre FROM Color c ORDER BY c.nombre ASC")
+    List<String> findDistinctNombres();
 }
